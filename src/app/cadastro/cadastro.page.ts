9317bfd5c1e771f4ab6a32cc9c45f8a02e8d9a5c
Terceiros Anos <ricsonc2eventos@outlook.com>
2023-08-16 21:56:19 -0300

    Meu primeiro commit

---
+++ src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+   public materia:string = '';
+   public descricao:string = '';
+   public datahora:string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+ 
+  salvar() { //void opcional//
+    alert('A matéria digitada foi' + this.materia);
+     
+  }
+
+}
